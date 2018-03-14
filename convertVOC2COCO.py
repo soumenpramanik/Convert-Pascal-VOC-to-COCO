93e275dde7ee012fde6a359c0da25b698807c3d6
Soumen Pramanik <soumenpramanik14@gmail.com>
2018-03-15 00:44:22 +0100

    Add files via upload
    Simple python script for conversion to coco format from voc format with segmentation.

--- convertVOC2COCO.py
+++ convertVOC2COCO.py
@@ -11,27 +11,29 @@ from collections import OrderedDict
 #xmlfile = "000023.xml"
 
 
-def generateVOC2Json(rootDir,testXMLFiles):
+def generateVOC2Json(rootDir,xmlFiles):
 	attrDict = dict()
 	#images = dict()
 	#images1 = list()
-	#class name in the images.
 	attrDict["categories"]=[{"supercategory":"none","id":1,"name":"header"},
-			        		{"supercategory":"none","id":2,"name":"row"},
-			        		{"supercategory":"none","id":3,"name":"logo"},
-			        		{"supercategory":"none","id":4,"name":"item_name"},
-							{"supercategory":"none","id":5,"name":"item_desc"},
-							{"supercategory":"none","id":6,"name":"price"},
-							{"supercategory":"none","id":7,"name":"total_price_text"},
-							{"supercategory":"none","id":8,"name":"total_price"},
-							{"supercategory":"none","id":9,"name":"footer"}
-							]
+			        {"supercategory":"none","id":2,"name":"row"},
+			        {"supercategory":"none","id":3,"name":"logo"},
+			        {"supercategory":"none","id":4,"name":"item_name"},
+				{"supercategory":"none","id":5,"name":"item_desc"},
+				{"supercategory":"none","id":6,"name":"price"},
+				{"supercategory":"none","id":7,"name":"total_price_text"},
+				{"supercategory":"none","id":8,"name":"total_price"},
+				{"supercategory":"none","id":9,"name":"footer"}
+			      ]
 	images = list()
 	annotations = list()
 	for root, dirs, files in os.walk(rootDir):
-		for file in testXMLFiles:
+		image_id = 0
+		for file in xmlFiles:
+			image_id = image_id + 1
 			if file in files:
-
+				
+				#image_id = image_id + 1
 				annotation_path = os.path.abspath(os.path.join(root, file))
 				
 				#tree = ET.parse(annotation_path)#.getroot()
@@ -46,10 +48,9 @@ def generateVOC2Json(rootDir,testXMLFiles):
 				image['width'] = int(doc['annotation']['size']['width'])
 				#keyList.append("width")
 
-
-
-
-				image['id'] = str(doc['annotation']['filename']).split('.jpg')[0]
+				#image['id'] = str(doc['annotation']['filename']).split('.jpg')[0]
+				image['id'] = image_id
+				print "File Name: {} and image_id {}".format(file, image_id)
 				images.append(image)
 				# keyList.append("id")
 				# for k in keyList:
@@ -58,10 +59,6 @@ def generateVOC2Json(rootDir,testXMLFiles):
 				# print images2
 				#print images
 
-				
-
-
-				
 				#attrDict["images"] = images
 
 				#print attrDict
@@ -71,23 +68,34 @@ def generateVOC2Json(rootDir,testXMLFiles):
 					for obj in doc['annotation']['object']:
 						for value in attrDict["categories"]:
 							annotation = dict()
-							if str(obj['name']) in value["name"]:
+							#if str(obj['name']) in value["name"]:
+							if str(obj['name']) == value["name"]:
 								#print str(obj['name'])
-								annotation["segmentation"] = []
+								#annotation["segmentation"] = []
 								annotation["iscrowd"] = 0
-								annotation["image_id"] = str(doc['annotation']['filename']).split('.jpg')[0] #attrDict["images"]["id"]
-								x1 = int(obj["bndbox"]["xmin"]) - 1
+								#annotation["image_id"] = str(doc['annotation']['filename']).split('.jpg')[0] #attrDict["images"]["id"]
+								annotation["image_id"] = image_id
+								x1 = int(obj["bndbox"]["xmin"])  - 1
 								y1 = int(obj["bndbox"]["ymin"]) - 1
 								x2 = int(obj["bndbox"]["xmax"]) - x1
 								y2 = int(obj["bndbox"]["ymax"]) - y1
 								annotation["bbox"] = [x1, y1, x2, y2]
-								annotation["area"] = int(x2 * y2)
+								annotation["area"] = float(x2 * y2)
 								annotation["category_id"] = value["id"]
 								annotation["ignore"] = 0
 								annotation["id"] = id1
+								annotation["segmentation"] = [[x1,y1,x1,(y1 + y2), (x1 + x2), (y1 + y2), (x1 + x2), y1]]
 								id1 +=1
 
 								annotations.append(annotation)
+				
+				else:
+					print "File: {} doesn't have any object".format(file)
+				#image_id = image_id + 1
+				
+			else:
+				print "File: {} not found".format(file)
+			
 
 	attrDict["images"] = images	
 	attrDict["annotations"] = annotations
@@ -95,17 +103,17 @@ def generateVOC2Json(rootDir,testXMLFiles):
 
 	#print attrDict
 	jsonString = json.dumps(attrDict)
-	with open("voc_train.json", "w") as f:
+	with open("receipts_valid.json", "w") as f:
 		f.write(jsonString)
 
-# rootDir = "./OBJECT_DETECTION/detectron/lib/datasets/data/VOC/Annotations"
+# rootDir = "/netscratch/pramanik/OBJECT_DETECTION/detectron/lib/datasets/data/Receipts/Annotations"
 # for root, dirs, files in os.walk(rootDir):
 # 	for file in files:
 # 		if file.endswith(".xml"):
 # 			annotation_path = str(os.path.abspath(os.path.join(root,file)))
 # 			#print(annotation_path)
 # 			generateVOC2Json(annotation_path)
-trainFile = "./OBJECT_DETECTION/train.txt"
+trainFile = "/netscratch/pramanik/OBJECT_DETECTION/valid.txt"
 trainXMLFiles = list()
 with open(trainFile, "rb") as f:
 	for line in f:
@@ -114,5 +122,5 @@ with open(trainFile, "rb") as f:
 		trainXMLFiles.append(fileName + ".xml")
 
 
-rootDir = "./OBJECT_DETECTION/detectron/lib/datasets/data/VOC/Annotations_xml"
+rootDir = "/netscratch/pramanik/OBJECT_DETECTION/detectron_old/lib/datasets/data/Receipts/Annotations_xml"
 generateVOC2Json(rootDir, trainXMLFiles)
